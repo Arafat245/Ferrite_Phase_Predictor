6110d78f9e8d6d88a025c0cc2474fcd6cda791f0
Arafat245 <arafat245@outlook.com>
2025-05-09 23:48:45 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+from our import our_calc
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    try:
+        # Get form input and convert to floats
+        inputs = {k: float(request.form[k]) for k in ['C', 'N', 'Mn', 'Ni', 'Cr', 'Mo', 'Si', 'Cu', 'Nb']}
+        result = our_calc(**inputs)
+        return jsonify({'ferrite_percent': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
---
+++ our.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+
+def our_calc(C, N, Mn, Ni, Cr, Mo, Si, Cu, Nb):
+    
+    if C <= 0.08:
+        # print('C less than 0.08')
+        Nieq = Ni + 35*C + 20*N + 0.5*Mn + 0.25*Cu
+        Creq = Cr + Mo + 0.7*Nb
+    else:
+        # print('C greater than 0.08')
+        Nieq = Ni + 7*C + 7*N + 0.5*Mn + 0.25*Cu
+        Creq = Cr + Mo + 0.7*Nb
+        
+    # print(f"According to Our Nieq = {Nieq}")
+    # print(f"According to Our Creq = {Creq}")
+        
+        
+    def ferrite_our(Nieq, Creq, a, b):
+        return a*((Nieq+2) / (Creq-5.4)) + b
+
+    data = {
+    "a": [-55.6, -41.7, -64.5, -190.5, -296.3, -121.2],
+    "b": [60.6, 46.7, 66.8, 158.1, 223.7, 138.8],
+    "FE (vol%) min": [0, 5, 10, 20, 40, 80],
+    "FE (vol%) max": [5, 10, 20, 40, 80, 100]
+    }
+
+    abdata = pd.DataFrame(data)
+    
+    for index, row in abdata.iterrows():
+        fe_our = ferrite_our(Nieq, Creq, row['a'], row['b'])
+        fe_our = int(fe_our)
+        
+        if row['FE (vol%) min'] <= fe_our and fe_our <= row['FE (vol%) max']:
+            #print(f'According to Our Calculation Ferrite: {fe_our}')
+            return fe_our
