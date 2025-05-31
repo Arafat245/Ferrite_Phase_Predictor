936a0207f47eef12315ae9dedc5dcc0adcb11969
Arafat <arafatrahman245@outlook.com>
2025-05-31 00:08:56 -0400

    aa

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-from flask import Flask, request, jsonify, render_template
-from our import our_calc
-from sc import sc_calc
-
-app = Flask(__name__)
-
-@app.route('/')
-def form():
-    return render_template('form.html')
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    try:
-        # Get form input and convert to floats
-        inputs = {k: float(request.form[k]) for k in ['C', 'N', 'Mn', 'Ni', 'Cr', 'Mo', 'Si', 'Cu', 'Nb']}
-        our_result, oNieq, oCreq = our_calc(**inputs)
-        sc_result,  sNieq, sCreq = sc_calc(**inputs)
-        return jsonify({
-            'ferrite_percent': our_result,
-            'sc_percent': sc_result,
-            'oNieq': oNieq,
-            'oCreq': oCreq,
-            'sNieq': sNieq,
-            'sCreq': sCreq,
-        })
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-=======
 from flask import send_file, Flask, request, jsonify, render_template
 from our import our_calc
 from sc import sc_calc
